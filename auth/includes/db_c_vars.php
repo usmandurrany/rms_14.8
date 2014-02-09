9a12a0f7ab9cd788c69b63809d558943e2ed3f97
Usman <usmandgreat@gmail.com>
2014-02-09 14:34:29 +0500

    Initial Commit
    Initialized Repo.
    Added SQL backup "ubit.sql"

---
+++ auth/includes/db_c_vars.php
@@ -0,0 +1,8 @@
+<?php
+	$databasehost='localhost';
+	$databaseusername='root';
+	$databasepassword='';
+	$databasename='ubit';
+	$db = mysql_connect($databasehost,$databaseusername,$databasepassword,$databasename) or die('Could not connect to databse, Please run <a href="../setup.php">SETUP</a>! ' . mysql_error());
+	mysql_select_db('ubit',	$db);
+?>
