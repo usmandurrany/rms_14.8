57e100c0891ba554fa74295d553fe18dfc5fa171
Usman <usmandgreat@gmail.com>
2014-02-09 19:35:27 +0000

    fixed connection prams

--- auth/includes/db_c_vars.php
+++ auth/includes/db_c_vars.php
@@ -1,7 +1,7 @@
 <?php
 	$databasehost='localhost';
 	$databaseusername='root';
-	$databasepassword='';
+	$databasepassword='iamamuslim';
 	$databasename='ubit';
 	$db = mysql_connect($databasehost,$databaseusername,$databasepassword,$databasename) or die('Could not connect to databse, Please run <a href="../setup.php">SETUP</a>! ' . mysql_error());
 	mysql_select_db('ubit',	$db);
