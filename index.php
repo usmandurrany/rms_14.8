34fac58f4a02e02bc7294031b4477236980edbbc
FaizanDurrani <coolfazanmastyvalla@gmail.com>
2014-02-10 22:50:10 +0500

    Login page Fix
    created a login page with basic html.

--- index.php
+++ index.php
@@ -1,122 +1,151 @@
 <?php require_once ("auth/includes/connect.php"); ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html lang="en">
 <head>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css' />
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta name="description" content="Result Managment System - Login" />
-<meta name="keywords" content="RMS, Result Managment System, Login" />
-<!--<link rel="stylesheet" type="text/css" href="css/style_fixed.css" />-->
-<link rel="stylesheet" type="text/css" href="css/style_fluid.css" />
-<link href="css/login.css" rel="stylesheet" type="text/css">
-<link href="css/jquery.ui.all.css" rel="stylesheet" type="text/css">
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.core.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.widget.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.tabs.min.js"></script>
-<script type="text/javascript" src="js/jquery.ui.datepicker.min.js"></script>
-<script type="text/javascript" src="js/jlogin.js"></script>
-<script type="text/javascript">
-$(document).ready(function()
-{
-   var jQueryTabs1Opts =
-   {
-      fx:
-      {
-         opacity: 'toggle',
-         duration: 'normal'
-      },
-      event: 'click',
-      deselectable: 'false'
-   };
-   $("#jQueryTabs1").tabs(jQueryTabs1Opts);
-  
-     var jQueryDatePicker1Opts =
-   {
-      dateFormat: 'dd/mm/yy',
-      changeMonth: true,
-      changeYear: true,
-      showButtonPanel: false,
-      showAnim: 'show'
-   };
-   $("#jQueryDatePicker1").datepicker(jQueryDatePicker1Opts);
-   $("#jQueryDatePicker1").datepicker("setDate", "new Date()");
-  
-});
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
+    <title>Result management system.</title>
 
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-</script>
-<title>Result Management System</title>
+    <!-- Custom styles for this template -->
+    <style>
+        body {
+      padding-top: 50px;
+      padding-bottom: 20px;
+    }
+    </style>
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
 
-<div id="top">
+<body style="">
 
-<div id="header">
-<h1>Login</h1>
-</div>
-<div class="clear"> </div>
-</div>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <form class="navbar-form navbar-right" role="form">
+                    <div class="form-group">
+                        <input type="text" placeholder="Email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" placeholder="Password" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-success">Sign in</button>
+                </form>
+            </div>
+            <!--/.navbar-collapse -->
+        </div>
+    </div>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+        <div class="container">
+            <div class="col-md-4" style="border-right: 1px solid #b8b8b8">
+                <form class="form" role="form">
+                    <h3><u>Student Login</u></h3>
+                    
+                    
+                       <p> Epnum:<br>
+                        <input type="text" class="form-control"/></p>
+                    
+                    
+                       <p> Password:<br>
+                        <input type="password" class="form-control"/></p>
+               
+                    <button type="submit" class="btn pull-right">Sign in</button>
+                </form>
+            </div>
+            <div class="col-md-4" style="border-right: 1px solid #b8b8b8">
+                <form class="form" role="form">
+                    <h3><u>Faculty/Admin Login</u></h3>
+                    
+                    
+                       <p> Username:<br>
+                        <select class="form-control"><option value="admin">Administrator</option><option value="faculty">Faculty</option></select></p>
+                    
+                    
+                       <p> Password:<br>
+                        <input type="password" class="form-control"/></p>
+               
+                    <button type="submit" class="btn pull-right">Sign in</button>
+                </form>
+                </div>
+                <div class="col-md-4">
+                
+                    <p><h4><u>Announcement #1</u></h4>
+                    <blockquote>Portal now under development.<small>Admin</small></blockquote></p>
+               
+                </div>
+            <div class="col-md-4">
+                
+                    <p><h4><u>Announcement #2</u></h4>
+                    <blockquote>Setting up Envoirment for developing portal.<small>Admin</small></blockquote></p>
+               
+                </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Example row of columns -->
+        <div class="row">
+            <div class="col-md-4">
+                
+                    <p><h4><u>Announcement #2</u></h4>
+                    <blockquote>Setting up Envoirment for developing portal.<small>Admin</small></blockquote></p>
+               
+                </div>
+            <div class="col-md-4">
+                
+                    <p><h4><u>Announcement #2</u></h4>
+                    <blockquote>Setting up Envoirment for developing portal.<small>Admin</small></blockquote></p>
+               
+                </div>
+            <div class="col-md-4">
+                
+                    <p><h4><u>Announcement #2</u></h4>
+                    <blockquote>Setting up Envoirment for developing portal.<small>Admin</small></blockquote></p>
+               
+                </div>
+        </div>
+
+        <hr>
+
+        <footer>
+            <p>© Company 2014</p>
+        </footer>
+    </div>
+    <!-- /container -->
 
 
-<div id="contentwrap">
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-new.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 
-<div class = "cleft">
-<h3>Student Login:</h3>
-<div id="sidebar">
-<form id="form_login" name="login" action="auth/includes/process.php" method="post">Enrolment No:<br><input type="text" value="" name="epnum"><br>Password:<br><input type="password" value="" name="password"><br><input id="form_submit_normal" type="submit" value="Login" name="login"></form>
-</div>
-
-<h3>Admin / Faculty Login</h3>
-<div id="sidebar">
-<form id="form_login" name="login" action="auth/includes/process.php" method="post">Username:<br><select name="username"><option value="administrator" selected="selected">Administrator</option><option value="faculty">Faculty</option></select><br>Password:<br><input type="password" value="" name="password"><br><input id="form_submit_normal" type="submit" value="Login" name="login"></form>
-</div>
-
-</div>
-<div class="cright">
-<div style="width:410px; margin:30px auto; text-align:center;">
-<img src="images/rms.png" id="Image1" alt="" border="0" style="width:188px;height:100px; margin:auto;">
-</div>
-<h2>Announcements</h2>
-<?php $data = show_news('system');
-while($news = mysql_fetch_array($data)){
-echo '<div align="left">'.$news[2].'</div>';
-}?>
-</div>
-</div>
-<div class="clear"> </div>
-
-</div>
-
-<div id="footer">
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20003234-4']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-<div class="left">
-Copyright &copy; 2012 by <a href="#">RMS</a>
-</div>
-
-<div class="right"> 
-<!-- Please consider leaving the credit - link back to our website in the footer intact or if you want to remove it please buy link back free license on http://www.simpletemplates.org/simple-website-template-2.html
--->
-Design by <a href="http://www.facebook.com/usmandgreat">Usman Durrani</a>
-</div>
-
-<div class="clear"> </div>
-
-</div>
 
 </body>
+
 </html>
